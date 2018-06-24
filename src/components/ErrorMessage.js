e4d7a2fb9eeae8e715b670c245d3cd603b719815
Richard Bach <rich.bach@osi.ca.gov>
2018-06-23 21:55:52 -0700

    testing ErrorMessage.js

--- src/components/ErrorMessage.js
+++ src/components/ErrorMessage.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 const ErrorMessage = ({msg}) => {
   return (
     <div style={{display : ((!msg || 0 === msg.length) ? 'none' : 'block')}} id="loginErrorMessage" className="errorMessage-customizable">
-        <div  >{msg} </div>
+        <div  >{msg}</div>
     </div>
   );
 };
